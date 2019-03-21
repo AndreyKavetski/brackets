600222015a4df9504c182c9bfdeb5df9ac846ac2
Andrey Kavetski <andreykavecki@gmail.com>
2019-03-21 21:46:04 +0300

    fix: task is complete

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
+function reseter(st, mass) {
+    for (let i = 0; i < st.length - 1; i++)
+        for (let j = 0; j < mass.length; j++) {
+            if (st[i] == mass[j][0] && st[i + 1] == mass[j][1]) {
+                st = st.slice(0, i) + st.slice(i + 2);
+                return st;
+            }
+        }
+    return st;
+}
 module.exports = function check(str, bracketsConfig) {
     let mass = arguments[1];
     let st = arguments[0];
-    let col = Array(mass.length);
-    let last = [];
-    let ind = 0;
-
-    for (let i = 0; i < col.length; i++) {
-        col[i] = 0;
-    }
-    for (let i = 0; i < st.length; i++) {
-        for (let j = 0; j < mass.length; j++) {
-            if (st[i] == mass[j][0]) {
-                col[j]++;
-                last.push(mass[j][1]);
-                ind++;
-            }
-            if (st[i] == mass[j][1] && col[j] > 0 && last[ind - 1] == st[i]) {
-                col[j]--;
-                ind--;
-                last.splice(last.indexOf(last[ind]), 1);
-            }
-        }
-    }
-    console.log(col);
-    console.log(st);
-    for (let i = 0; i < col.length; i++) {
-        if (col[i] != 0) return false;
+    while (st.length > 0) {
+        if (st == reseter(st, mass)) return false;
+        st = reseter(st, mass);
     }
     return true;
 }
